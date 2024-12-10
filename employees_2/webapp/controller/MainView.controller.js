0d01c23e549973ae331a7bc137e3d88f630a225f
KarinaAgz <karinaangon10@gmail.com>
2024-12-10 01:10:51 +0000

    first commit 04

--- employees_2/webapp/controller/MainView.controller.js
+++ employees_2/webapp/controller/MainView.controller.js
@@ -1,10 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], function (Controller)  {
     "use strict";
 
-    return Controller.extend("logaligroup.employees.controller.MainView", {
-        onInit() {
+    var Main =Controller.extend("logaligroup.employees.controller.MainView", {});
+        
+    Main.prototype.onValidate=function(){
+        var inputEmployee=this.byId("inputEmployee");
+        var valueEmployee=inputEmployee.getValue();
+
+        if(valueEmployee.length ===6){
+            //inputEmployee.setDescription("ok");
+            this.byId("labelCountry").setVisible(true);
+            this.byId("slCountry").setVisible(true);
+        }else{
+            //inputEmployee.setDescription("Not ok");
+            this.byId("labelCountry").setVisible(false);
+            this.byId("slCountry").setVisible(false);
         }
+    };
+    return Main;
     });
-});
